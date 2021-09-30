32c2eae4ba1f6872ad8d3aafe217f9c8182477fd
uki <uki@c-fo.com>
2021-09-30 11:26:23 +0900

    Add attach_tmux_session_if_needed function

--- shell/fish/config.fish
+++ shell/fish/config.fish
@@ -1,3 +1,23 @@
+function attach_tmux_session_if_needed
+  set ID (tmux list-sessions)
+  if test -z "$ID"
+    tmux new-session
+    return
+  end
+
+  set new_session "Create New Session"
+  set ID (echo $ID\n$new_session | peco --on-cancel=error | cut -d: -f1)
+  if test "$ID" = "$new_session"
+    tmux new-session
+  else if test -n "$ID"
+    tmux attach-session -t "$ID"
+  end
+end
+
+if test -z $TMUX
+  attach_tmux_session_if_needed
+end
+
 set -xg LC_CTYPE en_US.UTF-8
 set -xg LC_ALL en_US.UTF-8
 set -xg LANG en_US.UTF-8
