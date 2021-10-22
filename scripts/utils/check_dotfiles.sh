c600a77f9180980d40486ccff0d35b4f30fa2407
Yuki Shimizu <yucchini.yuki1014@gmail.com>
2021-10-22 16:17:03 +0900

    Add check_dotfiles.sh

---
+++ scripts/utils/check_dotfiles.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -ue
+
+source ~/dotfiles/scripts/utils/has.sh
+
+DOTFILES_DIR="$HOME/dotfiles"
+DOT_TARBALL="https://github.com/uki1014/dotfiles/tarball/master"
+DOT_REMOTE_URL="https://github.com/uki1014/dotfiles.git"
+
+# Install my dotfile
+check_dotfiles() {
+  if [ ! -d $DOTFILES_DIR ]; then
+    echo "Downloading dotfiles..."
+
+    mkdir $DOTFILES_DIR
+
+    if has "git"; then
+      # The directory to clone needs to be empty,
+      # so you'll need to create a dotfiles directory first, then clone the contents into that directory.
+      git clone ${DOT_REMOTE_URL} ${DOTFILES_DIR}
+    else
+      curl -fsSLo ${HOME}/dotfiles.tar.gz ${DOT_TARBALL}
+      tar -zxf ${HOME}/dotfiles.tar.gz --strip-components 1 -C ${DOTFILES_DIR}
+      rm -f ${HOME}/dotfiles.tar.gz
+    fi
+
+    echo $(tput setaf 2)Download dotfiles complete!. ✔︎$(tput sgr0)
+    cd $DOTFILES_DIR
+  else
+    echo "you're Dotfiles has been already installed."
+  fi
+}
+
+# If you want to run check_dotfiles, pass something as an argument
+[ ! $# == 0 ] && check_dotfiles
