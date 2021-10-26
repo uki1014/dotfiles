728c68bf875965bd4f55245246f9cffc75618952
uki <yuki-shimizu@c-fo.com>
2021-10-26 15:45:48 +0900

    update git.sh

--- scripts/lib/git.sh
+++ scripts/lib/git.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -ue
 
 create_token_file() {
-  ln -s ~/dotfiles/git/.git-templates ~/
+  git secrets --install ~/dotfiles/git/.git-templates/git-secrets
+  git config --global init.templatedir '~/dotfiles/git/.git-templates/git-secrets'
   git secrets --register-aws --global
   touch ~/dotfiles/git/token.fish
   echo 'Please set GITHUB_TOKEN and GITHUB_USERNAME in "~/dotfiles/git/token.fish"'
