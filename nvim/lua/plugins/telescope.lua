07b48180a944ea677b620c7a69170eac8b8e39a4
uki <yuki-shimizu@c-fo.com>
2022-04-04 22:50:18 +0900

    update telescope.nvim keymap

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -5,13 +5,13 @@ vim.api.nvim_set_keymap('n', ';f', ':Telescope find_files find_command=rg,--igno
 -- Grep string
 vim.api.nvim_set_keymap('n', ';r', ':Telescope live_grep find_command=rg,--ignore-case,--hidden,--files<CR>', opt)
 -- ?Searches for the string under my cursor in current working directory
-vim.api.nvim_set_keymap('n', ';e', ':Telescope grep_string find_command=rg,--ignore-case,--hidden,--files<CR>', opt)
+vim.api.nvim_set_keymap('n', ';;', ':Telescope grep_string find_command=rg,--ignore-case,--hidden,--files<CR>', opt)
 -- vim command history
 vim.api.nvim_set_keymap('n', ';t', ':lua require"telescope.builtin".command_history{}<CR>', opt)
 -- 現環境で使えるCommandのリスト
 vim.api.nvim_set_keymap('n', ';c', ':lua require"telescope.builtin".commands{}<CR>', opt)
-vim.api.nvim_set_keymap('n', ';g', ':Telescope buffers<CR>', opt)
-vim.api.nvim_set_keymap('n', ';;', ':Telescope help_tags<CR>', opt)
+vim.api.nvim_set_keymap('n', ';e', ':Telescope buffers<CR>', opt)
+vim.api.nvim_set_keymap('n', ';g', ':Telescope help_tags<CR>', opt)
 -- builtin command lists
 vim.api.nvim_set_keymap('n', ';b', ':lua require"telescope.builtin".builtin{}<CR>', opt)
 
