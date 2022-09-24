7031c5f4dbd204e66e804b0fc8018e708220cf92
uki <yuki-shimizu@c-fo.com>
2022-09-24 13:30:36 +0000

    update auto-session config

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -97,7 +97,7 @@ return packer.startup(function(use)
       require('auto-session').setup {
         auto_session_suppress_dirs = { '~/Projects', '~/Downloads', '~/' },
         log_level = 'error',
-        auto_session_enable_last_session = false,
+        auto_session_enable_last_session = true,
         auto_session_root_dir = vim.fn.stdpath('data') .. '/sessions/',
         auto_session_enabled = true,
         auto_save_enabled = true,
