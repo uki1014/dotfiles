9c35be5c9aacbb36b871a94aecf79bfc7141e46c
uki <yuki-shimizu@c-fo.com>
2022-09-24 04:59:24 +0000

    adjust telescope config

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -201,7 +201,6 @@ return packer.startup(function(use)
     },
     config = function()
       local telescope = require('telescope')
-      telescope.load_extension("file_browser")
       local actions = require('telescope.actions')
       local builtin = require('telescope.builtin')
       local file_browser_actions = telescope.extensions.file_browser.actions
@@ -210,13 +209,46 @@ return packer.startup(function(use)
         return vim.fn.expand('%:p:h')
       end
 
+      telescope.setup({
+        defaults = {
+          initial_mode = 'insert',
+          mappings = {
+            i = {
+              ['<Down>'] = actions.cycle_history_next,
+              ['<Up>'] = actions.cycle_history_prev,
+            },
+            n = {
+              ['q'] = actions.close
+            },
+          },
+        },
+        extensions = {
+          file_browser = {
+            theme = 'dropdown',
+            layout_config = {
+              width = 150,
+              height = 50
+            },
+            hijack_netrw = true,
+            mappings = {
+              n = {
+                ["N"] = file_browser_actions.create,
+                ['h'] = file_browser_actions.goto_parent_dir,
+              }
+            }
+          }
+        }
+      })
+
+      telescope.load_extension("file_browser")
+
       local function open_file_browser(initial_mode)
         telescope.extensions.file_browser.file_browser({
           dir_icon = "",
           path = "%:p:h",
           cwd = telescope_buffer_dir(),
           respect_gitignore = false,
-          hidden = false,
+          hidden = true,
           grouped = true,
           all_previewer = true,
           initial_mode = initial_mode,
@@ -328,36 +360,6 @@ return packer.startup(function(use)
       -- Tabs
       maps.nmap('fl', ':tabnext<Return>', maps.s)
       maps.nmap('fj', ':tabprev<Return>', maps.s)
-
-      telescope.setup({
-        defaults = {
-          initial_mode = 'insert',
-          mappings = {
-            i = {
-              ['<Down>'] = actions.cycle_history_next,
-              ['<Up>'] = actions.cycle_history_prev,
-            },
-            n = {
-              ['q'] = actions.close
-            },
-          },
-        },
-        extensions = {
-          file_browser = {
-            theme = 'dropdown',
-            layout_config = {
-              width = 150,
-            },
-            hijack_netrw = true,
-            mappings = {
-              n = {
-                ["N"] = file_browser_actions.create,
-                ['h'] = file_browser_actions.goto_parent_dir,
-              }
-            }
-          }
-        }
-      })
     end
   }
 
