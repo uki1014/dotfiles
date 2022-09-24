285f63c601cf1b628f74925a7ba4238614e4b5f5
uki <yuki-shimizu@c-fo.com>
2022-09-24 13:46:08 +0000

    use prettierd

--- nvim/plugin/null-ls.lua
+++ nvim/plugin/null-ls.lua
@@ -4,7 +4,7 @@ null_ls.setup({
   sources = {
     null_ls.builtins.diagnostics.eslint_d,
     null_ls.builtins.diagnostics.fish,
-    null_ls.builtins.formatting.prettier,
+    null_ls.builtins.formatting.prettierd,
     -- null_ls.builtins.diagnostics.rubocop.with {
     --   command = 'bundle',
     --   args = { 'exec', 'rubocop', '-f', 'json', '--stdin', '$FILENAME' },
