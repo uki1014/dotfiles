c77a845122891de18b703c13026d6ea918e4430f
uki <yuki-shimizu@c-fo.com>
2022-09-24 03:33:35 +0000

    Add nvim-treesitter config

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -30,6 +30,42 @@ return packer.startup(function(use)
     run = function()
       require('nvim-treesitter.install').update({ with_sync = true })
     end,
+    config = function()
+      require('nvim-treesitter.configs').setup({
+        highlight = {
+          enable = false,
+          disable = {},
+        },
+        indent = {
+          enable = true,
+          disable = {},
+        },
+        ensure_installed = {
+          "tsx",
+          "toml",
+          "fish",
+          "php",
+          "json",
+          "yaml",
+          "css",
+          "html",
+          "lua"
+        },
+        autotag = {
+          enable = true,
+        },
+      })
+
+      local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
+      parser_config.tsx.filetype_to_parsername = {
+        'javascript',
+        'javascript.jsx',
+        'javascriptreact',
+        'typescript',
+        'typescript.tsx',
+        'typescriptreact',
+      }
+    end
   }
 
   use {
