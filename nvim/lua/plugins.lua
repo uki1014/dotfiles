600fdaf3da0f23d43a54fe76ccc1b28b18d8b4c5
uki <yuki-shimizu@c-fo.com>
2022-09-24 09:48:11 +0000

    update auto-session config

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -85,8 +85,13 @@ return packer.startup(function(use)
     'rmagatti/auto-session',
     config = function()
       require("auto-session").setup {
+        auto_session_suppress_dirs = { '~/Projects', '~/Downloads', '~/' },
         log_level = "error",
-        auto_session_suppress_dirs = { "~/" },
+        auto_session_enable_last_session = false,
+        auto_session_root_dir = vim.fn.stdpath('data') .. '/sessions/',
+        auto_session_enabled = true,
+        auto_save_enabled = true,
+        auto_restore_enabled = true,
       }
     end
   }
