5701e7c978463353cbe47e8a6ce1bdc06109e1da
uki <yuki-shimizu@c-fo.com>
2022-09-24 13:46:02 +0000

    remove auto-session and add vim-workspace

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -91,18 +91,27 @@ return packer.startup(function(use)
       Maps.nmap('<Leader>a', ':PrevimOpen<CR>', Maps.n)
     end
   }
+  -- use {
+  --   'rmagatti/auto-session',
+  --   config = function()
+  --     require('auto-session').setup {
+  --       auto_session_suppress_dirs = { '~/Projects', '~/Downloads', '~/' },
+  --       log_level = 'error',
+  --       auto_session_enable_last_session = true,
+  --       auto_session_root_dir = vim.fn.stdpath('data') .. '/sessions/',
+  --       auto_session_enabled = true,
+  --       auto_save_enabled = true,
+  --       auto_restore_enabled = true,
+  --     }
+  --   end
+  -- }
   use {
-    'rmagatti/auto-session',
+    'thaerkh/vim-workspace',
     config = function()
-      require('auto-session').setup {
-        auto_session_suppress_dirs = { '~/Projects', '~/Downloads', '~/' },
-        log_level = 'error',
-        auto_session_enable_last_session = true,
-        auto_session_root_dir = vim.fn.stdpath('data') .. '/sessions/',
-        auto_session_enabled = true,
-        auto_save_enabled = true,
-        auto_restore_enabled = true,
-      }
+      vim.g.workspace_autocreate = '1'
+      vim.g.workspace_undodir = os.getenv('HOME')..'/.config/nvim/sessions/'
+      vim.g.workspace_autosave = '0'
+      vim.g.workspace_session_directory = os.getenv('HOME')..'/.config/nvim/sessions/'
     end
   }
   use {
