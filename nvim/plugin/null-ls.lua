275ce5f9ad05bb6eed42ebb211513d94966917d9
uki <yuki-shimizu@c-fo.com>
2022-09-26 19:28:10 +0900

    Disable rubocop formatter and Enable diagnostic only

--- nvim/plugin/null-ls.lua
+++ nvim/plugin/null-ls.lua
@@ -5,14 +5,14 @@ null_ls.setup({
 		null_ls.builtins.diagnostics.fish,
 		null_ls.builtins.formatting.stylua,
 		null_ls.builtins.formatting.prettierd,
-		-- null_ls.builtins.diagnostics.rubocop.with({
-		--   command = "bundle",
-		--   args = { "exec", "rubocop", "-f", "json", "--stdin", "$FILENAME" },
-		-- }),
-		null_ls.builtins.formatting.rubocop.with({
+		null_ls.builtins.diagnostics.rubocop.with({
 			command = "bundle",
-			args = { "exec", "rubocop", "--auto-correct", "-f", "quiet", "--stderr", "--stdin", "$FILENAME" },
+			args = { "exec", "rubocop", "-f", "json", "--stdin", "$FILENAME" },
 		}),
+		-- null_ls.builtins.formatting.rubocop.with({
+		--   command = "bundle",
+		--   args = { "exec", "rubocop", "--auto-correct", "-f", "quiet", "--stderr", "--stdin", "$FILENAME" },
+		-- }),
 	},
 	on_attach = function(client)
 		if client.server_capabilities.documentFormattingProvider then
