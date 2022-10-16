c7a6c1d5b7b01f892091cdcaf2efe0b5bd2078f6
uki <yuki-shimizu@c-fo.com>
2022-10-16 14:41:41 +0900

    Remove deprecated method of mason

--- nvim/plugin/lspconfig.lua
+++ nvim/plugin/lspconfig.lua
@@ -66,7 +66,7 @@ require("mason-lspconfig").setup({
 })
 require("mason-lspconfig").setup_handlers({
 	function()
-		local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+		local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 		lspconfig.flow.setup({
 			on_attach = on_attach,
