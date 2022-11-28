ca0664e95a3b6fd097fc6f16a703a9c2830eafcf
uki <yuki-shimizu@c-fo.com>
2022-11-28 10:00:27 +0900

    fix init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,12 @@
+local status, impatient = pcall(require, "impatient")
+if status then
+	impatient.enable_profile()
+end
+
 Maps = require("maps")
 
 require("keymap")
 require("plugins")
-require("impatient").enable_profile()
 require("config")
 
 vim.cmd([[autocmd BufWritePost plugins.lua PackerCompile]])
