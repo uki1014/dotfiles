092554d13cfb385852c089fb2f42f4b1f361bf8c
Yuki Shimizu <yuki.shimizu.me@gmail.com>
2023-05-07 13:35:30 +0900

    Remove LuaSnip version

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -18,7 +18,7 @@ require("lazy").setup({
 	{
 		"L3MON4D3/LuaSnip",
 		-- follow latest release.
-		version = "<CurrentMajor>.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+		-- version = "<CurrentMajor>.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
 		-- install jsregexp (optional!).
 		build = "make install_jsregexp",
 	},
