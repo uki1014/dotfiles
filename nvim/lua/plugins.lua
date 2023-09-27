36a6577561ac3fdea3d076581cbbc76b25e7f72d
Yuki Shimizu <yuki.shimizu.me@gmail.com>
2023-09-27 12:30:15 +0900

    Add highlight-undo.nvim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -71,6 +71,30 @@ require("lazy").setup({
 			})
 		end,
 	},
+	{
+
+		"tzachar/highlight-undo.nvim",
+		config = function()
+			require("highlight-undo").setup({
+				duration = 300,
+				undo = {
+					hlgroup = "HighlightUndo",
+					mode = "n",
+					lhs = "u",
+					map = "undo",
+					opts = {},
+				},
+				redo = {
+					hlgroup = "HighlightUndo",
+					mode = "n",
+					lhs = "<C-r>",
+					map = "redo",
+					opts = {},
+				},
+				highlight_for_count = true,
+			})
+		end,
+	},
 	"itchyny/lightline.vim",
 	-- Finder / Filer
 	"nvim-lua/plenary.nvim",
