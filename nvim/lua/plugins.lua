8b30da48c7791eca7e912f7508b591dfd1c31fd1
Yuki Shimizu <yuki.shimizu.contact@gmail.com>
2024-06-02 12:49:54 +0900

    Add nvim-ts-context-commentstring plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -224,9 +224,11 @@ require("lazy").setup({
 					line = ";e",
 					block = ";b",
 				},
+				pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
 			})
 		end,
 	},
+	"JoosepAlviste/nvim-ts-context-commentstring",
 	{
 		"uki1014/vim-zenspace",
 		config = function()
