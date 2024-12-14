a1d01bb84f787626797e5d5558921819d8ccd3b9
Yuki Shimizu <yuki.shimizu.contact@gmail.com>
2024-12-15 00:49:36 +0900

    update conform.nvim config

--- nvim/plugin/conform.lua
+++ nvim/plugin/conform.lua
@@ -1,16 +1,41 @@
 local conform = require("conform")
-local js_formatters =
-	{ "biome", "prettierd", "prettier", "yaml", "markdown", "css", "scss", "html", stop_after_first = true }
+
+local function biome_lsp_or_prettier(bufnr)
+	local has_biome_lsp = vim.lsp.get_clients({
+		bufnr = bufnr,
+		name = "biome",
+	})[1]
+	if has_biome_lsp then
+		return {}
+	end
+	local has_prettier = vim.fs.find({
+		-- https://prettier.io/docs/en/configuration.html
+		".prettierrc",
+		".prettierrc.json",
+		".prettierrc.yml",
+		".prettierrc.yaml",
+		".prettierrc.json5",
+		".prettierrc.js",
+		".prettierrc.cjs",
+		".prettierrc.toml",
+		"prettier.config.js",
+		"prettier.config.cjs",
+	}, { upward = true })[1]
+	if has_prettier then
+		return { "prettierd", stop_after_first = true }
+	end
+	return { "biome", stop_after_first = true }
+end
 
 conform.setup({
 	formatters_by_ft = {
 		lua = { "stylua" },
-		json = js_formatters,
-		javascript = js_formatters,
-		javascriptreact = js_formatters,
-		typescript = js_formatters,
-		typescriptreact = js_formatters,
-		astro = js_formatters,
+		json = biome_lsp_or_prettier,
+		javascript = biome_lsp_or_prettier,
+		javascriptreact = biome_lsp_or_prettier,
+		typescript = biome_lsp_or_prettier,
+		typescriptreact = biome_lsp_or_prettier,
+		astro = biome_lsp_or_prettier,
 	},
 })
 
