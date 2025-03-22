48fd3d36de2ba9abea97bff20eb4d02330e94e11
Yuki Shimizu <yuki.shimizu.contact@gmail.com>
2025-03-22 18:22:59 +0900

    update fish config

--- shell/fish/config.fish
+++ shell/fish/config.fish
@@ -1,3 +1,18 @@
+set fish_cursor_insert block
+
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
+
 function attach_tmux_session_if_needed
   if [ -z $TMUX ]
     set ID (tmux list-sessions)
